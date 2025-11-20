e5453c74221cf3db3527ce6aa7e64cc812344fb1
devhimal <himalhobbs.364@gmail.com>
2025-11-20 09:34:41 +0545

    feat: added the template of portfolio website  in python

---
+++ portfolio-app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+
+def login():
+    return render_template('login.html')
+
+
+def home():
+    projects = ["mobile app", "website redesign", "data analysis"]
+    return render_template('index.html', projects=projects)
+
+if __name__ == '__main__':
+    app.run(debug=True)
